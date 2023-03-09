6328109639a4dca4ccd9703e20df0aff63e1ca6f
rick huang <rickyhuang0824@gmail.com>
2023-03-09 15:02:22 -0800

    Dynamic style with CSS module

--- courseGoalProject/src/components/UI/Button/Button.js
+++ courseGoalProject/src/components/UI/Button/Button.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './Button.css';
+import styles from "./Button.module.css";
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+const Button = (props) => {
+    return (
+        <button
+            type={props.type}
+            className={styles.button}
+            onClick={props.onClick}
+        >
+            {props.children}
+        </button>
+    );
 };
 
 export default Button;
